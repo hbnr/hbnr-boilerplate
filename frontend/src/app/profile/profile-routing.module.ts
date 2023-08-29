4f61535144d4c7e7787da37bf3e1ccbd2e30de1b
Sammy Hubner <smmy@hbnr.nl>
2023-08-29 16:10:25 +0200

    --wip-- auth module

--- frontend/src/app/profile/profile-routing.module.ts
+++ frontend/src/app/profile/profile-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {authGuard} from 'src/app/auth/auth.guard';
 import {ProfileComponent} from 'src/app/profile/profile.component';
 
 const routes: Routes = [
-    {path: '', component: ProfileComponent}
+    {path: '', component: ProfileComponent, canActivate: [authGuard]}
 ]
 
 @NgModule({
--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-sign-up',
@@ -8,9 +8,11 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class SignUpComponent {
     form = new FormGroup({
-        email: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
         password: new FormControl(null, [Validators.required]),
         passwordConfirm: new FormControl(null, [Validators.required]),
+    }, {
+        validators: this.passwordMatchValidator
     });
 
     submit() {
@@ -19,5 +21,20 @@ export class SignUpComponent {
         } else {
             console.log('success');
         }
+    }
+
+    passwordMatchValidator(): ValidatorFn {
+        return (formGroup: AbstractControl): ValidationErrors | null => {
+            const passwordControl = formGroup.get('password')!;
+            const passwordConfirmControl = formGroup.get('passwordConfirm')!;
+
+            if (passwordControl.value !== passwordConfirmControl.value) {
+                passwordConfirmControl.setErrors({passwordMismatch: true});
+                return {passwordMismatch: true};
+            } else {
+                passwordConfirmControl.setErrors(null);
+                return null;
+            }
+        };
     }
 }
